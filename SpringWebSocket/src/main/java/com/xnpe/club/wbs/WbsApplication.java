8ea69fdfba8dc518e0f136e0db6ce2c1e137c0e1
chazz <408217395@qq.com>
2021-07-02 15:13:07 +0800

    初始化

---
+++ SpringWebSocket/src/main/java/com/xnpe/club/wbs/WbsApplication.java
@@ -0,0 +1,14 @@
+package com.xnpe.club.wbs;
+
+import com.xnpe.club.wbs.config.WebSocketConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbsApplication {
+
+    public static void main(String[] args) {
+        //TODO
+        SpringApplication.run(WbsApplication.class, args);
+    }
+}
